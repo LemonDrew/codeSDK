4dabe26d12d83d28ccbd5024a4fb8bebfcc20823
Derrick Wong Liang Jun <derrickwong8909@gmail.com>
2025-09-06 17:33:19 +0800

    add new condition

--- routes/gambit.py
+++ routes/gambit.py
@@ -17,22 +17,27 @@ def gambit():
         prev_front = None
 
         for number, amount in intel:
-            if current_mp >= amount and current_stamina > 0:
-                if prev_front != number:
+            if prev_front != number:
+                total_time += 10
+                prev_front = number
+
+            remaining_mp_needed = amount
+
+            while remaining_mp_needed > 0:
+                if current_mp == 0 or current_stamina == 0:
                     total_time += 10
-                current_mp -= amount
+                    current_mp = reserve
+                    current_stamina = stamina
+
+                used_mp = min(current_mp, remaining_mp_needed)
+                current_mp -= used_mp
                 current_stamina -= 1
+                remaining_mp_needed -= used_mp
 
-            else:
-                total_time += 10
-                current_mp = reserve
-                current_stamina = stamina
-
-                current_mp -= amount
-                current_stamina -= 1
-                total_time += 10
-
-            prev_front = number
+                if remaining_mp_needed > 0 and (current_mp == 0 or current_stamina == 0):
+                    total_time += 10
+                    current_mp = reserve
+                    current_stamina = stamina
 
         total_time += 10
 
