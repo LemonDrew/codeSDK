d6460ab320529167989277dbce950a87bf02ed0b
Derrick Wong Liang Jun <derrickwong8909@gmail.com>
2025-09-06 13:09:25 +0800

    Update safeguard

--- routes/safeguard.py
+++ routes/safeguard.py
@@ -1,17 +1,18 @@
 import re
 from flask import Flask, request, jsonify
 from routes import app
-import logging
+# import logging
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
 def process_challenge_one(data):
+
     transform_map = {
-        "encode_mirror_alphabet": encode_mirror_alphabet,
-        "double_consonants": double_consonants,
+        "encode_mirror_alphabet": decode_mirror_alphabet,
+        "double_consonants": remove_double_consonants,
         "mirror_words": mirror_words,
         "swap_pairs": swap_pairs,
-        "encode_index_parity": encode_index_parity
+        "encode_index_parity": decode_index_parity
     }
 
     transformations = data["transformations"]
@@ -20,8 +21,6 @@ def process_challenge_one(data):
         func_names = re.findall(r'(\w+)\(x\)', transformations)
     elif isinstance(transformations, list):
         func_names = transformations
-    else:
-        raise TypeError("transformations must be a string or list")
 
     encrypted_data = data["transformed_encrypted_word"]
 
@@ -46,7 +45,7 @@ def mirror_words(x):
     return (" ").join(result)
 
 
-def encode_mirror_alphabet(x):
+def decode_mirror_alphabet(x):
     result = []
 
     for ch in x:
@@ -80,70 +79,98 @@ def toggle_case(x):
 
 def swap_pairs(x):
     words = x.split(" ")
-    swapped_words = []
+    reversed_words = []
 
     for word in words:
         chars = list(word)
-        for i in range(0, len(chars) - 1, 2):
+        last_char = None
+
+    
+        if len(chars) % 2 != 0:
+            last_char = chars[-1]
+            chars = chars[:-1]
+
+        for i in range(0, len(chars), 2):
             chars[i], chars[i + 1] = chars[i + 1], chars[i]
-        swapped_words.append("".join(chars))
 
-    return " ".join(swapped_words)
+        if last_char:
+            chars.append(last_char)
+
+        reversed_words.append("".join(chars))
+
+    return " ".join(reversed_words)
 
 
-def encode_index_parity(x):
+
+def decode_index_parity(x):
     words = x.split(" ")
-    transformed_words = []
+    reversed_words = []
 
     for word in words:
-        even_chars = [word[i] for i in range(0, len(word), 2)]
-        odd_chars = [word[i] for i in range(1, len(word), 2)]
-        transformed_words.append("".join(even_chars + odd_chars))
+        n = len(word)
+        half = (n + 1) // 2  
+        even_chars = word[:half]
+        odd_chars = word[half:]
 
-    return " ".join(transformed_words)
+        original = []
+        for i in range(n):
+            if i % 2 == 0:
+                original.append(even_chars[i // 2])
+            else:
+                original.append(odd_chars[i // 2])
+        
+        reversed_words.append("".join(original))
 
-def double_consonants(x):
+    return " ".join(reversed_words)
+
+
+def remove_double_consonants(x):
     vowels = set("aeiouAEIOU")
     result = []
+    i = 0
 
-    for ch in x:
-        if ch.isalpha() and ch not in vowels: 
-            result.append(ch * 2)
+    while i < len(x):
+        result.append(x[i])
+        if x[i].isalpha() and x[i] not in vowels: 
+            i += 2
         else:  
-            result.append(ch)
+            i += 1
 
     return "".join(result)
 
 
 @app.route("/operation-safeguard", methods=["POST"])
 def operation_safeguard():
-    # data = request.get_json()
-    # if data is None:
-    #     return jsonify({"error": "Invalid JSON"}), 400
+    
+    data = request.get_json()
 
-    # challenge_one_data = data.get("challenge_one")
-    # if challenge_one_data is None:
-    #     return jsonify({"error": "Missing challenge_one"}), 400
+    challenge_one_data = data.get("challenge_one")
+    challenge_two_data = data.get("challenge_two")
+    challenge_three_data = data.get("challenge_three")
+    challenge_four_data = data.get("challenge_four")
 
-    # result_one = process_challenge_one(challenge_one_data)
+    result_one = process_challenge_one(challenge_one_data)
+    
 
-    # result_two = "value_from_challenge_2"
-    # result_three = "value_from_challenge_3"
-    # result_four = "final_decrypted_value"
+    if data is None:
+        return jsonify({"error": "Invalid JSON"}), 400
 
-    # return jsonify({
-    #     "challenge_one": result_one,
-    #     "challenge_two": result_two,
-    #     "challenge_three": result_three,
-    #     "challenge_four": result_four
-    # })
+    challenge_one_data = data.get("challenge_one")
+    if challenge_one_data is None:
+        return jsonify({"error": "Missing challenge_one"}), 400
+
+    result_one = process_challenge_one(challenge_one_data)
+
+    result_two = "value_from_challenge_2"
+    result_three = "value_from_challenge_3"
+    result_four = "final_decrypted_value"
+
     return jsonify({
-        "challenge_one": "1",
-        "challenge_two": "2",
-        "challenge_three": "3",
-        "challenge_four": "4"
+        "challenge_one": result_one,
+        "challenge_two": result_two,
+        "challenge_three": result_three,
+        "challenge_four": result_four
     })
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
-
