d0b09f6ad31e670975aac9f415e2ea4111894824
SQ77 <andrewzsq@gmail.com>
2025-09-06 15:15:14 +0800

    Update trivia

--- routes/trivia.py
+++ routes/trivia.py
@@ -5,15 +5,31 @@ from routes import app
 @app.route('/trivia', methods=['GET'])
 def get_trivia_answers():
     answers = [
-        3,  # Q1: Educated guess 
+        2,  # Q1: Educated guess 
         1,  # Q2
         2,  # Q3
         2,  # Q4
         3,  # Q5
         4,  # Q6: Amy Winehouse (never did a James Bond theme)
-        2,  # Q7: 0.5px (smallest reasonable font size)
+        3,  # Q7: 0.5px (smallest reasonable font size)
         1,  # Q8: "graceful the pet" (anagram of "capture the flag")
-        3   # Q9: USA, Hong Kong, Singapore
+        3,   # Q9: USA, Hong Kong, Singapore
+        3, #10
+        3, #11
+        3, #12
+        4, #13,
+        2,
+        2, #15
+        2,
+        1,
+        2,
+        1,
+        3, #20
+        2,
+        2,
+        2,
+        5,
+        2 #25
     ]
     
     return jsonify({
